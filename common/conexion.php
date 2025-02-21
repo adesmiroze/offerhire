d3581ed3dd93cc1d6036abdf5acc0e5da0752a18
Adesmiro <adesmiroze@gmail.com>
2025-02-20 23:04:20 -0500

    x

--- common/conexion.php
+++ common/conexion.php
@@ -6,10 +6,8 @@ $usuario = "u232828735_offerhire"; // Tu nombre de usuario de MySQL
 $contrasena = "[QBzMx4h"; // Tu contraseña de MySQL
 $base_de_datos = "u232828735_offerhire"; // El nombre de tu base de datos
 
-// Crear conexión
 $conexion = new mysqli($host, $usuario, $contrasena, $base_de_datos);
 
-// Verificar la conexión
 if ($conexion->connect_error) {
   die("Error de conexión: " . $conexion->connect_error);
 }else{ echo"Conexion exitosa";}
@@ -17,10 +15,6 @@ if ($conexion->connect_error) {
 // Opcional: Establecer el conjunto de caracteres
 $conexion->set_charset("utf8");
 
-// La conexión está establecida, puedes continuar con tus consultas y operaciones
-// ...
-
-// Cerrar la conexión (se recomienda hacerlo al finalizar las operaciones)
 $conexion->close();
 
 ?>

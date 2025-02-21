b3016b10a92d18840d8e4411fdc1ab7e0ba37a5f
Adesmiro <107264145+adesmiroze@users.noreply.github.com>
2025-02-21 13:06:04 -0500

    p1

--- web/login/login.php
+++ web/login/login.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>OfferHire/Login</title>
-  <link rel="icon" type="image/png" href="../../assets/images/favicon.png" sizes="16x16">
+  <link rel="icon" type="image/png" href="../../assets/propio/favicon.png" sizes="16x16">
   <!-- remix icon font css  -->
   <link rel="stylesheet" href="../../assets/css/remixicon.css">
   <!-- BootStrap css -->

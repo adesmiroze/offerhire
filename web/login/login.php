9c7ca1ced8ad554f41c88988299ef2b6449b5816
Adesmiro <adesmiroze@gmail.com>
2025-02-23 22:00:21 -0500

    l2

--- web/login/login.php
+++ web/login/login.php
@@ -4,38 +4,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>OfferHire/Login</title>
-  <link rel="icon" type="image/png" href="../../assets/propio/favicon.png" sizes="16x16">
-  <!-- remix icon font css  -->
-  <link rel="stylesheet" href="../../assets/css/remixicon.css">
-  <!-- BootStrap css -->
-  <link rel="stylesheet" href="../../assets/css/lib/bootstrap.min.css">
-  <!-- Apex Chart css -->
-  <link rel="stylesheet" href="../../assets/css/lib/apexcharts.css">
-  <!-- Data Table css -->
-  <link rel="stylesheet" href="../../assets/css/lib/dataTables.min.css">
-  <!-- Text Editor css -->
-  <link rel="stylesheet" href="../../assets/css/lib/editor-katex.min.css">
-  <link rel="stylesheet" href="../../assets/css/lib/editor.atom-one-dark.min.css">
-  <link rel="stylesheet" href="../../assets/css/lib/editor.quill.snow.css">
-  <!-- Date picker css -->
-  <link rel="stylesheet" href="../../assets/css/lib/flatpickr.min.css">
-  <!-- Calendar css -->
-  <link rel="stylesheet" href="../../assets/css/lib/full-calendar.css">
-  <!-- Vector Map css -->
-  <link rel="stylesheet" href="../../assets/css/lib/jquery-jvectormap-2.0.5.css">
-  <!-- Popup css -->
-  <link rel="stylesheet" href="../../assets/css/lib/magnific-popup.css">
-  <!-- Slick Slider css -->
-  <link rel="stylesheet" href="../../assets/css/lib/slick.css">
-  <!-- prism css -->
-  <link rel="stylesheet" href="../../assets/css/lib/prism.css">
-  <!-- file upload css -->
-  <link rel="stylesheet" href="../../assets/css/lib/file-upload.css">
-  
-  <link rel="stylesheet" href="../../assets/css/lib/audioplayer.css">
-  <!-- main css -->
-  <link rel="stylesheet" href="../../assets/css/style.css">
+  <title><?php echo "$title"; ?></title>
+  <?php include_once"../../assets/css.php"; ?>
 </head>
   <body>
 
@@ -104,34 +74,7 @@
     </div>
 </section>
 
-  <!-- jQuery library js -->
-  <script src="assets/js/lib/jquery-3.7.1.min.js"></script>
-  <!-- Bootstrap js -->
-  <script src="assets/js/lib/bootstrap.bundle.min.js"></script>
-  <!-- Apex Chart js -->
-  <script src="assets/js/lib/apexcharts.min.js"></script>
-  <!-- Data Table js -->
-  <script src="assets/js/lib/dataTables.min.js"></script>
-  <!-- Iconify Font js -->
-  <script src="assets/js/lib/iconify-icon.min.js"></script>
-  <!-- jQuery UI js -->
-  <script src="assets/js/lib/jquery-ui.min.js"></script>
-  <!-- Vector Map js -->
-  <script src="assets/js/lib/jquery-jvectormap-2.0.5.min.js"></script>
-  <script src="assets/js/lib/jquery-jvectormap-world-mill-en.js"></script>
-  <!-- Popup js -->
-  <script src="assets/js/lib/magnifc-popup.min.js"></script>
-  <!-- Slick Slider js -->
-  <script src="assets/js/lib/slick.min.js"></script>
-  <!-- prism js -->
-  <script src="assets/js/lib/prism.js"></script>
-  <!-- file upload js -->
-  <script src="assets/js/lib/file-upload.js"></script>
-  <!-- audioplayer -->
-  <script src="assets/js/lib/audioplayer.js"></script>
-  
-  <!-- main js -->
-  <script src="assets/js/app.js"></script>
+  <?php include_once "../../assets/js.php";?>
 
 <script>
       // ================== Password Show Hide Js Start ==========
